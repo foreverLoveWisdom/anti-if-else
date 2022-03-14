d0ed7522299c2b6734b4b1042e2f72ba5e0c1416
tien-do-hanko <76943319+tien-do-hanko@users.noreply.github.com>
2022-03-14 19:20:06 +0700

    Add the first test to accept the current truth

--- gilded_rose_test.rb
+++ gilded_rose_test.rb
@@ -10,6 +10,16 @@ class TestUntitled < Minitest::Test
     assert_equal items[0].name, 'foo'
   end
 
+  def test_backstage_pass
+    assert_backstage_pass_quality(22, 8, 20)
+  end
+
+  def assert_backstage_pass_quality(expected, sell_in, quality)
+    items = [Item.new('Backstage passes to a TAFKAL80ETC concert', sell_in, quality)]
+    GildedRose.new(items).update_quality
+    assert_equal expected, items[0].quality
+  end
+
   def test_report
     report_lines = []
     items = [
